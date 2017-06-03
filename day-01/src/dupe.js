bd135aad52015bbe891d99641dc703214a05dd2a
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2017-06-03 03:28:52 -0300

    Simplified stuff

--- day-01/src/dupe.js
+++ day-01/src/dupe.js
@@ -1,9 +1,18 @@
-const defaultFunction = (x, y) => x === y;
-const isDuplicate = (wasDuplicate, x1, i1, x2, i2, fn) => wasDuplicate || (i2 < i1 && fn(x1, x2));
+const strictEquals = (x, y) => x === y;
 
-const dupe = (arr, fn = defaultFunction) => arr.reduce((acc, x, i) => {
-	if (arr.reduce((acc, x2, i2) => isDuplicate(acc, x, i, x2, i2, fn), false)) acc.push(x);
-	return acc;
-}, []);
+const makeFilterer = (f, composer) => (acc, x, i, xs) => f(acc, x, i, xs) ? composer(acc, x) : acc;
+
+const or = (x, y) => x || y;
+const concatArray = (arr, x) => {
+	arr.push(x);
+	return arr;
+};
+const lessThan = (x, y) => x < y;
+
+const makeInnerReduce = (xs, x, i, equals) => xs.reduce(makeFilterer((_, x2, i2) => lessThan(i2, i) && equals(x, x2), or), false);
+
+const makeOuterReduce = (xs, equals) => xs.reduce(makeFilterer((_, x, i) => makeInnerReduce(xs, x, i, equals), concatArray), []);
+
+const dupe = (arr, equals = strictEquals) => makeOuterReduce(arr, equals);
 
 module.exports = dupe;
